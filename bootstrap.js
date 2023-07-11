da95b541574b926766bac4bbe40263eda8be73cf
Diana_Ailyn <utel.alum.diana@gmail.com>
2023-07-10 23:13:38 -0600

    Ultimos cambios

--- bootstrap.js
+++ bootstrap.js
@@ -1,10 +1,9 @@
 document.addEventListener(function() {
     document.getElementById('myForm').addEventListener('submit', function(event) {
     document.getElementById('enviarBtn').addEventListener('click', function(event) {
-      event.preventDefault(); // Prevenir el comportamiento predeterminado del botón
       alert('Enviado!');
     document.getElementById('limpiarBtn').addEventListener('click', function(event) {
-      event.preventDefault(); // Prevenir el comportamiento predeterminado del botón
+      event.preventDefault();
     });
   });
   
